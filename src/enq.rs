ef98ec3f16bc293cee5fdb4029c4413e16388208
zjedna <45286809+zjedna@users.noreply.github.com>
2019-01-09 13:21:28 +0100

    Update enq.rs

--- src/enq.rs
+++ src/enq.rs
@@ -4176,7 +4176,7 @@ pub fn q_5_0_enthalpy(reactions:&Vec<Reaction>)->(String,String) {
 }
 
 //work out Keq from equilibrium concentrations.
-//UNFINISHED.
+//FINISHEDISH.
 
 pub fn q_5_1(reactions:&Vec<Reaction>)->(String,String) {
 	
